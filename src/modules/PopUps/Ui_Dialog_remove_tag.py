4ae2cf42a02c3c5fa58e585f9851fee6c4b5cc5e
Lucie OUVRIER-BUFFET <lucie.ouvrier-buffet@grenoble-inp.org>
2018-02-28 17:38:35 +0100

    debug remove tag search
    Former-commit-id: 0dfb72d82f1005e730da6aa73c885a39a7929540

--- src/modules/PopUps/Ui_Dialog_remove_tag.py
+++ src/modules/PopUps/Ui_Dialog_remove_tag.py
@@ -81,13 +81,13 @@ class Ui_Dialog_remove_tag(QDialog):
 
         if str_search != "":
             return_list = []
-            for tag in self.database.getTags():
+            for tag in self.database.getUserTags():
                 tag_name = tag.tag
                 if str_search.upper() in tag_name.upper():
                     return_list.append(tag_name)
         else:
             return_list = []
-            for tag in self.database.getTags():
+            for tag in self.database.getUserTags():
                 tag_name = tag.tag
                 return_list.append(tag_name)
         self.list_widget_tags.clear()
