dd28fe3f191c22f5d0392291c9ee07675cee156a
harbined <david.harbine@grenoble-inp.org>
2018-03-06 16:09:34 +0100

    Working from the scripts directory, closes #67
    Former-commit-id: ee69cad0cfe50aa07783d13cc88c45b70e5eb306

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*- # Character encoding, recommended
 
 import sys
+import os
 from PyQt5.QtWidgets import QApplication
 from MainWindow.Main_Window import Main_Window
 from DataBase.DataBase import DataBase
 
 if __name__ == '__main__':
+    # Working from the scripts directory
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
     app = QApplication(sys.argv)
 
