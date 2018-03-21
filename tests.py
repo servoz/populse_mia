bfafab900a986571ea15467619303cd78aa850dc
Lucie OUVRIER-BUFFET <lucie.ouvrier-buffet@grenoble-inp.org>
2018-03-21 17:28:40 +0100

    tests
    Former-commit-id: eb0988063189ad64711c678a0aa3f10b0f322efb

--- tests.py
+++ tests.py
@@ -2,10 +2,11 @@ import os
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-from src.modules.Database.Database import Database
+from Database.Database import Database
 
 def test_database_creation():
     pass
+    # raise Exception("test")
 
 total_tests = 0
 failed_tests = []
@@ -18,7 +19,7 @@ except Exception:
 
 if len(failed_tests) > 0:
     errorMessage = "Some tests failed.\n"
-    errorMessage += len(failed_tests) + "/" + total_tests  + " tests failed."
+    errorMessage += str(len(failed_tests)) + "/" + str(total_tests)  + " tests failed.\n"
     errorMessage += "Here is a list of the tests that failed:\n"
     for failed in failed_tests:
         errorMessage += "\t" + failed + "\n"
