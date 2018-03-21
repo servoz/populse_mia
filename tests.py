5d501dd65c5deac329bbfec5f1c1676542471c40
Lucie OUVRIER-BUFFET <lucie.ouvrier-buffet@grenoble-inp.org>
2018-03-21 16:39:15 +0100

    tests
    Former-commit-id: 4f1ee0e535b1f3cbd8b948a036ac6756d101395c

--- tests.py
+++ tests.py
@@ -1,6 +1,21 @@
-def test_database():
-    print("test database")
-    assert True == False
+from Database.Database import Database
 
-print("tests to fill")
-test_database()
+def test_database_creation():
+    pass
+
+total_tests = 0
+failed_tests = []
+
+try:
+    total_tests += 1
+    test_database_creation()
+except Exception:
+    failed_tests.append("database_creation")
+
+if len(failed_tests) > 0:
+    errorMessage = "Some tests failed.\n"
+    errorMessage += len(failed_tests) + "/" + total_tests  + " tests failed."
+    errorMessage += "Here is a list of the tests that failed:\n"
+    for failed in failed_tests:
+        errorMessage += "\t" + failed + "\n"
+    raise Exception(errorMessage)
