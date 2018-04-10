22b8b67f8d3a4511e031d581e8bd1cb92fde41b7
dhrbn <davidharbine@gmail.com>
2018-04-10 11:44:38 +0200

    Made the process library inherit from a QTreeView to handle draggin with the correct mime data

--- process_library.py
+++ process_library.py
@@ -7,13 +7,14 @@ import pkgutil
 # PyQt / PySide import, via soma
 from soma.qt_gui import qt_backend
 #qt_backend.set_qt_backend('PySide', pyqt_api=2, compatible_qt5=True)
-from soma.qt_gui.qt_backend.QtCore import Qt, Signal
+from soma.qt_gui.qt_backend.QtCore import Qt, Signal, QModelIndex, \
+    QAbstractItemModel, QByteArray, QMimeData
 from soma.qt_gui.qt_backend.Qt import QWidget, QTreeWidget, QLabel, \
     QPushButton, QDialog, QTreeWidgetItem, QHBoxLayout, QVBoxLayout, \
-    QLineEdit, QApplication, QSplitter, QFileDialog
+    QLineEdit, QApplication, QSplitter, QFileDialog, QTreeView
 
-"""# CAPSUL import
-from capsul.api import get_process_instance, StudyConfig"""
+# CAPSUL import
+from capsul.api import get_process_instance, StudyConfig
 
 # Soma import
 from soma.path import find_in_path
@@ -33,8 +34,11 @@ class ProcessLibraryWidget(QWidget):
         self.update_config()
 
         # Process Library
-        self.process_library = ProcessLibrary(self.packages)
-        self.process_library.itemClicked.connect(self.display_parameters)
+        self.process_library = DictionaryTreeWidget(self.packages)
+        self.process_library.setDragDropMode(self.process_library.DragOnly)
+        self.process_library.setAcceptDrops(False)
+        self.process_library.setDragEnabled(True)
+        self.process_library.setSelectionMode(self.process_library.SingleSelection)
 
         # Push button to call the package library
         push_button_pkg_lib = QPushButton()
@@ -53,7 +57,6 @@ class ProcessLibraryWidget(QWidget):
         h_box = QHBoxLayout()
         h_box.addWidget(self.splitter)
         h_box.addWidget(push_button_pkg_lib)
-        #h_box.addWidget(self.label_test)
 
         self.setLayout(h_box)
 
@@ -61,6 +64,12 @@ class ProcessLibraryWidget(QWidget):
         self.pkg_library.signal_save.connect(self.update_process_library)
 
     def display_parameters(self, item):
+        """ This method was used to display the parameters of a process.
+        It will be useful to generate processes in the near future.
+        """
+
+        study_config = StudyConfig(modules=StudyConfig.default_modules + ['NipypeConfig'])
+
         process_name = item.text(0)
         list_path = []
         while item is not self.process_library.topLevelItem(0):
@@ -75,32 +84,16 @@ class ProcessLibraryWidget(QWidget):
 
         for k, v in sorted(list(pkg.__dict__.items())):
             if k == process_name:
-                txt = "Inputs: \n"
-                if v.input_spec is not None:
-                    txt += str(v.input_spec())
-                txt2 = "\nOutputs: \n"
-                if v.output_spec is not None:
-                    txt2 += str(v.output_spec())
+                try:
+                    process = get_process_instance(v)
+                except:
+                    print('AIEEEE')
+                    pass
+                else:
+                    print(process.get_inputs())
+                txt = "Inputs: \n" + str(v.input_spec())
+                txt2 = "\nOutputs: \n" + str(v.output_spec())
                 self.label_test.setText(txt + txt2)
-                print(v)
-                obj = v.input_spec()
-                print(obj)
-                print(list(obj.__dict__.keys()))
-                for attr in list(obj.__dict__.keys()):
-                    print('##########')
-                    print(attr)
-                    real_attr = getattr(obj, attr)
-                    try:
-                        attr.get_value(attr, name='mandatory    ')
-                    except:
-                        pass
-                    else:
-                        print('YOLOOOOOOOOOOOOOOOOOOOOOOOOO0000000000000000000000O')
-                        print(attr.get(name='mandatory'))
-                    if hasattr(real_attr, 'optional'):
-                        print('#############################################################')
-                    print(type(real_attr))
-                    print(real_attr)
 
     def update_config(self):
         self.process_config = self.load_config()
@@ -109,7 +102,7 @@ class ProcessLibraryWidget(QWidget):
     def update_process_library(self):
         self.update_config()
         self.process_library.package_tree = self.packages
-        self.process_library.generate_tree()
+        self.process_library.load_dictionary(self.packages)
 
     def open_pkg_lib(self):
         self.pkg_library.show()
@@ -137,62 +130,305 @@ class ProcessLibraryWidget(QWidget):
         with open('process_config.yml', 'w', encoding='utf8') as stream:
             yaml.dump(self.process_config, stream, default_flow_style=False, allow_unicode=True)
 
+class Node(object):
 
-class ProcessLibrary(QTreeWidget):
-    ''' A library that displays all the available processes.
-    '''
-    def __init__(self, package_tree):
-        """ Generate the library.
-        """
-        super(ProcessLibrary, self).__init__()
-        self.setHeaderLabel("Available processes")
-        self.package_tree = package_tree
-        self.generate_tree()
+    def __init__(self, name, parent=None):
+        self._name = name
+        self._parent = parent
+        self._children = []
+        self._value = None
+        if parent is not None:
+            parent.addChild(self)
 
-    def generate_tree(self):
-        self.clear()
-        self.fill_item(self.invisibleRootItem(), self.package_tree)
+    def typeInfo(self):
+        return 'NODE'
 
-    def fill_item(self, item, value):
-        item.setExpanded(True)
+    def addChild(self, child):
+        self._children.append(child)
 
-        if type(value) is dict:
-            for key, val in sorted(value.items()):
-                child = QTreeWidgetItem()
-                child.setText(0, str(key))
-                if type(val) is dict:
-                    item.addChild(child)
-                    self.fill_item(child, val)
-                else:
-                    if val == 'process_enabled':
-                        item.addChild(child)
+    def insertChild(self, position, child):
+        if position < 0 or position > len(self._children):
+            return False
 
-        elif type(value) is list:
-            for val in value:
-                child = QTreeWidgetItem()
-                item.addChild(child)
-                if type(val) is dict:
-                    child.setText(0, '[dict]')
-                    self.fill_item(child, val)
-                elif type(val) is list:
-                    child.setText(0, '[list]')
-                    self.fill_item(child, val)
-                else:
-                    child.setText(0, str(val))
+        self._children.insert(position, child)
+        child._parent = self
+        return True
 
-                child.setExpanded(True)
+    def removeChild(self, position, child):
+        if position < 0 or position > len(self._children):
+            return False
 
+        self._children.pop(position)
+        child._parent = None
+        return True
+
+    def attrs(self):
+        classes = self.__class__.__mro__
+        keyvalued = {}
+        for cls in classes:
+            for key, value in cls.items():
+                if isinstance(value, property):
+                    keyvalued[key] = value.fget(self)
+        return keyvalued
+
+    def to_list(self):
+        output = []
+        if self._children:
+            for child in self._children:
+                output += [self.name, child.to_list()]
         else:
-            if value == 'process_enabled':
-                child = QTreeWidgetItem()
-                child.setText(0, str(value))
-                item.addChild(child)
+            output += [self.name, self.value]
+        return output
 
-    def add_package(self, package):
-        pass
+    def to_dict(self, d={}):
+        for child in self._children:
+            child._recurse_dict(d)
+        return d
 
-    def remove_package(self, package):
-        pass
+    def _recurse_dict(self, d):
+        if self._children:
+            d[self.name] = {}
+            for child in self._children:
+                child._recurse_dict(d[self.name])
+        else:
+            d[self.name] = self.value
+
+    def name():
+        def fget(self):
+            return self._name
+        def fset(self, value):
+            self._name = value
+        return locals()
+    name = property(**name())
+
+    def value():
+        def fget(self):
+            return self._value
+        def fset(self, value):
+            self._value = value
+        return locals()
+    value = property(**value())
+
+    def child(self, row):
+        return self._children[row]
+
+    def childCount(self):
+        return len(self._children)
+
+    def parent(self):
+        return self._parent
+
+    def row(self):
+        if self._parent is not None:
+            return self._parent._children.index(self)
+
+    def log(self, tabLevel=-1):
+        output = ''
+        tabLevel += 1
+
+        for i in range(tabLevel):
+            output += '    '
+
+        output += ''.join(('|----', self._name,' = ', '\n'))
+
+        for child in self._children:
+            output += child.log(tabLevel)
+
+        tabLevel -= 1
+        output += '\n'
+        return output
+
+    def __repr__(self):
+        return self.log()
+
+    def data(self, column):
+        if column is 0:
+            parent = self._parent
+            text = self.name
+            while parent.name != 'Root':
+                text = parent.name + '.' + text
+                parent = parent._parent
+            return text #self.name
+
+        elif column is 1:
+            return self.value
+
+    def setData(self, column, value):
+        if column is 0:
+            self.name = value
+        if column is 1:
+            self.value = value
+
+    def resource(self):
+        return None
+
+class DictionaryTreeModel(QAbstractItemModel):
+    """Data model providing a tree of an arbitrary dictionary"""
+
+    def __init__(self, root, parent=None):
+        super(DictionaryTreeModel, self).__init__(parent)
+        self._rootNode = root
+
+    def mimeTypes(self):
+        return ['component/name']
+
+    def mimeData(self, idxs):
+        mimedata = QMimeData()
+        for idx in idxs:
+            if idx.isValid():
+                node = idx.internalPointer()
+                txt = node.data(idx.column())
+                mimedata.setData('component/name', QByteArray(txt.encode()))
+                print(txt)
+        return mimedata
+
+    def rowCount(self, parent):
+        """the number of rows is the number of children"""
+        if not parent.isValid():
+            parentNode = self._rootNode
+        else:
+            parentNode = parent.internalPointer()
+
+        return parentNode.childCount()
+
+    def columnCount(self, parent):
+        """Number of columns is always 1 """
+        return 1
+
+    def data(self, index, role):
+        """returns the data requested by the view"""
+        if not index.isValid():
+            return None
+
+        node = index.internalPointer()
+
+        if role == Qt.DisplayRole or role == Qt.EditRole:
+            return node.name
+
+    def setData(self, index, value, role=Qt.EditRole):
+        """this method gets called when the user changes data"""
+        if index.isValid():
+            if role == Qt.EditRole:
+                node = index.internalPointer()
+                node.setData(index.column(), value)
+                return True
+        return False
+
+    def headerData(self, section, orientation, role):
+        """returns the name of the requested column"""
+        if role == Qt.DisplayRole:
+            if section == 0:
+                return "Packages"
+            if section == 1:
+                return "Value"
+
+    def flags(self, index):
+        """everything is enable and selectable. Only the leaves can be dragged. """
+        node = index.internalPointer()
+        if node.childCount() > 0:
+            return Qt.ItemIsEnabled | Qt.ItemIsSelectable
+        else:
+            return Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled
+
+    def parent(self, index):
+        """returns the parent from given index"""
+        node = self.getNode(index)
+        parentNode = node.parent()
+        if parentNode == self._rootNode:
+            return QModelIndex()
+
+        return self.createIndex(parentNode.row(), 0, parentNode)
+
+    def index(self, row, column, parent):
+        """returns an index from given row, column and parent"""
+        parentNode = self.getNode(parent)
+        childItem = parentNode.child(row)
+
+        if childItem:
+            return self.createIndex(row, column, childItem)
+        else:
+            return QModelIndex()
+
+    def getNode(self, index):
+        """returns a Node() from given index"""
+        if index.isValid():
+            node = index.internalPointer()
+            if node:
+                return node
+
+        return self._rootNode
+
+    def insertRows(self, position, rows, parent=QModelIndex()):
+        """insert rows from starting position and number given by rows"""
+        parentNode = self.getNode(parent)
+
+        self.beginInsertRows(parent, position, position + rows - 1)
+
+        for row in range(rows):
+            childCount = parentNode.childCount()
+            childNode = Node("untitled" + str(childCount))
+            success = parentNode.insertChild(position, childNode)
+
+        self.endInsertRows()
+        return success
+
+    def removeRows(self, position, rows, parent=QModelIndex()):
+        """remove the rows from position to position+rows"""
+        parentNode = self.getNode(parent)
+        self.beginRemoveRows(parent, position, position + rows - 1)
+
+        for row in range(rows):
+            success = parentNode.removeChild(position)
+
+        self.endRemoveRows()
+        return success
+
+    def to_dict(self):
+        return self._rootNode.to_dict()
+
+
+def node_structure_from_dict(datadict, parent=None, root_node=None):
+    """returns a hierarchical node stucture required by the TreeModel"""
+    if not parent:
+        root_node = Node('Root')
+        parent = root_node
+
+    for name, data in sorted(datadict.items()):
+        if isinstance(data, dict):
+            node = Node(name, parent)
+            node = node_structure_from_dict(data, node, root_node)
+        else:
+            if data == 'process_enabled':
+                node = Node(name, parent)
+                node.value = data
+
+    return root_node
+
+class DictionaryTreeWidget(QTreeView):
+    """returns an object containing the tree of the given dictionary d.
+    example:
+    tree = DictionaryTree(d)
+    tree.edit()
+    d_edited = tree.dict()
+    d_edited contains the dictionary with the edited data.
+    this has to be refactored...
+    """
+
+    def __init__(self, d):
+        super(DictionaryTreeWidget, self).__init__()
+        self.load_dictionary(d)
+
+    def load_dictionary(self,d):
+        """load a dictionary into my tree application"""
+        self._d = d
+        self._nodes = node_structure_from_dict(d)
+        self._model = DictionaryTreeModel(self._nodes)
+        self.setModel(self._model)
+        self.expandAll()
+
+    def to_dict(self):
+        """returns a dictionary from the tree-data"""
+        return self._model.to_dict()
 
 
 class PackageLibraryDialog(QDialog):
@@ -486,3 +722,64 @@ if __name__ == "__main__":
     plw = ProcessLibraryWidget()
     plw.show()
     sys.exit(app.exec_())
+
+
+
+################# OLD VERSION OF THE TREE VIEW (WITH A QTREEWIDGET)
+'''class ProcessLibrary(QTreeWidget):
+    """ A library that displays all the available processes.
+    """
+    def __init__(self, package_tree):
+        """ Generate the library.
+        """
+        super(ProcessLibrary, self).__init__()
+        self.setHeaderLabel("Available processes")
+        self.package_tree = package_tree
+        self.generate_tree()
+
+    def generate_tree(self):
+        self.clear()
+        self.fill_item(self.invisibleRootItem(), self.package_tree)
+
+    def fill_item(self, item, value):
+        item.setExpanded(True)
+
+        if type(value) is dict:
+            for key, val in sorted(value.items()):
+                child = QTreeWidgetItem()
+                child.setText(0, str(key))
+                if type(val) is dict:
+                    item.addChild(child)
+                    self.fill_item(child, val)
+                else:
+                    if val == 'process_enabled':
+                        item.addChild(child)
+
+        elif type(value) is list:
+            for val in value:
+                child = QTreeWidgetItem()
+                item.addChild(child)
+                if type(val) is dict:
+                    child.setText(0, '[dict]')
+                    self.fill_item(child, val)
+                elif type(val) is list:
+                    child.setText(0, '[list]')
+                    self.fill_item(child, val)
+                else:
+                    child.setText(0, str(val))
+
+                child.setExpanded(True)
+
+        else:
+            if value == 'process_enabled':
+                child = QTreeWidgetItem()
+                child.setText(0, str(value))
+                item.addChild(child)
+
+    def add_package(self, package):
+        pass
+
+    def remove_package(self, package):
+        pass
+
+'''
