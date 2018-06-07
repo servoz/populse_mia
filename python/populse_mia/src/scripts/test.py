91b447b3f24219ecee577a5b5eeda48d0acefd34
Lucie OUVRIER-BUFFET <lucie.ouvrier-buffet@grenoble-inp.org>
2018-06-07 17:09:17 +0200

    testing travis

--- python/populse_mia/src/scripts/test.py
+++ python/populse_mia/src/scripts/test.py
@@ -1,7 +1,7 @@
 import unittest
 from PyQt5.QtTest import QTest
 from PyQt5.QtWidgets import QApplication
-from Project.Project import Project, COLLECTION_CURRENT
+from Project.Project import Project, COLLECTION_CURRENT, COLLECTION_INITIAL
 from MainWindow.Main_Window import Main_Window
 
 class TestMIADataBrowser(unittest.TestCase):
@@ -23,6 +23,10 @@ class TestMIADataBrowser(unittest.TestCase):
         self.assertTrue("FileName" in tags)
         self.assertTrue("Type" in tags)
         self.assertEqual(self.imageViewer.project.database.get_documents_names(COLLECTION_CURRENT), [])
+        collections = self.imageViewer.project.database.get_collections_names()
+        self.assertEqual(len(collections), 2)
+        self.assertTrue(COLLECTION_INITIAL in collections)
+        self.assertTrue(COLLECTION_CURRENT in collections)
 
 if __name__ == '__main__':
     unittest.main()
