36c020bf6669847511041c251976815f526d618c
Lucie OUVRIER-BUFFET <lucie.ouvrier-buffet@grenoble-inp.org>
2018-06-07 15:07:38 +0200

    first test

--- python/populse_mia/src/scripts/test.py
+++ python/populse_mia/src/scripts/test.py
@@ -1,9 +1,22 @@
 import unittest
+from PyQt5.QtTest import QTest
+from PyQt5.QtWidgets import QApplication
+from Project.Project import Project
+from MainWindow.Main_Window import Main_Window
 
 class TestMIADataBrowser(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def setUp(self):
+        self.app = QApplication([])
+        self.project = Project(None, True)
+        self.imageViewer = Main_Window(self.project)
+
+    def tearDown(self):
+        pass
+
+    def test_unnamed_project_software_opening(self):
+        self.assertIsInstance(self.project, Project)
+        self.assertEqual(self.imageViewer.project.getName(), "Unnamed project")
 
 if __name__ == '__main__':
     unittest.main()
