5d3197216be8bdb67128a2d9c679b9bcdca14db9
Eric Condamine <37933899+servoz@users.noreply.github.com>
2019-01-25 18:07:02 +0100

    Update pipeline_editor.py
    Very little change, just for the position of one capsul import

--- python/populse_mia/pipeline_manager/pipeline_editor.py
+++ python/populse_mia/pipeline_manager/pipeline_editor.py
@@ -17,12 +17,12 @@ from PyQt5 import QtGui, QtWidgets, QtCore
 
 # Capsul imports
 from capsul.api import get_process_instance, Process, PipelineNode, Switch
+from capsul.qt_gui.widgets.pipeline_developper_view import PipelineDevelopperView, NodeGWidget
 
 # soma-base imports
 from soma.utils.weak_proxy import weak_proxy
 
 # Populse_MIA imports
-from capsul.qt_gui.widgets.pipeline_developper_view import PipelineDevelopperView, NodeGWidget
 from populse_mia.pipeline_manager.node_controller import FilterWidget
 from populse_mia.pop_ups.pop_up_close_pipeline import PopUpClosePipeline
 from populse_mia.software_properties.config import Config
