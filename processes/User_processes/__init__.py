56dd4a6eda909e6177d2b3dd6abde496840f53f6
Eric Condamine <37933899+servoz@users.noreply.github.com>
2019-01-31 12:17:58 +0100

    Update __init__.py

--- processes/User_processes/__init__.py
+++ processes/User_processes/__init__.py
@@ -1,10 +1,1 @@
 
-from .test import Test
-from .segmentation import Segmentation
-from .segmentation2 import Segmentation2
-from .test2 import Test2
-from .Test import Test
-from .Test2 import Test2
-from .Segmentation import Segmentation
-from .Segmentation2 import Segmentation2
-from .Test3 import Test3
