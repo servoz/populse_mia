9daeb7f18756a502a4976e78543be30c146fbe5a
econdami <econdami@yahoo.fr>
2019-03-05 14:37:45 +0100

    Final commit for the yaml.load story ..

--- python/populse_mia/install_mia.py
+++ python/populse_mia/install_mia.py
@@ -221,8 +221,8 @@ class MIAInstallWidget(QtWidgets.QWidget):
     def load_config(config_file):
         with open(config_file, 'r') as stream:
             try:
-                # return = yaml.load(stream, Loader=yaml.FullLoader) ## from version 5.1
-                return  = yaml.load(stream) ## version < 5.1
+                # return yaml.load(stream, Loader=yaml.FullLoader) ## from version 5.1
+                return yaml.load(stream) ## version < 5.1
             
             except yaml.YAMLError as exc:
                 print(exc)
